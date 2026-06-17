682c6b0f808c7acf3b583d9f040a8c8d8f60bd12
Vladyslav <vbu@ewe.dk>
2026-06-17 12:48:36 +0200

    docker , yaml files

--- Application/Program.cs
+++ Application/Program.cs
@@ -19,17 +19,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+app.UseSwaggerUI(options =>
 {
-	app.UseSwagger();
-
-	app.UseSwaggerUI(options =>
-	{
-		options.SwaggerEndpoint("/swagger/v1/swagger.json", "Health Monitoring API v1");
-		options.RoutePrefix = string.Empty;
-	});
-}
-
+	options.SwaggerEndpoint("/swagger/v1/swagger.json", "Health Monitoring API v1");
+	options.RoutePrefix = string.Empty;
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
